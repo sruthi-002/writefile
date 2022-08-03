25a88e14d0723ffd1997eb0792eb8549cc247e26
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-03 16:37:58 +0530

    Add files via upload

---
+++ Writefile.java
@@ -0,0 +1,36 @@
+﻿package com.company;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.io.FileWriter;
+class createfile
+{
+    public static void newfile() throws IOException
+    {
+        Path path = Paths.get("Saibaba.txt");
+        try
+        {
+            Files.createFile(path);
+            System.out.println("File Created"+" "+path);
+            FileWriter write = new FileWriter("Saibaba.txt");
+            write.write("I am new to java file handling");
+            write.close();
+            System.out.println("Successfully Written");
+        }
+        catch(FileAlreadyExistsException e)
+        {
+            System.out.println("File Already Exists");
+            System.out.println("An error Ocurred");
+        }
+    }
+}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        createfile f = new createfile();
+        f.newfile();
+    }
+}
